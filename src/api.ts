1aa69abf1e4e91445c2fe20958b08935c57400da
iplam24 <vxlcontact143@gmail.com>
2026-06-07 15:23:54 +0700

    first commit

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const baseURL = window.location.hostname === 'localhost' 
   ? 'http://localhost:5000/api' 
-  : `http://${window.location.hostname}:5000/api`;
+  : 'https://thiepmoi-two.vercel.app/api';
 
 const api = axios.create({
   baseURL
